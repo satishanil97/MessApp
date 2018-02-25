7bf72e18b11be40ed86c9d2caa85ffe5163575a7
satishanil97 <satishanil97@gmail.com>
2018-02-25 19:59:16 +0530

    identifying student and contractor from email IDs.

--- app/src/main/java/com/example/satish/messapp/LoginActivity.java
+++ app/src/main/java/com/example/satish/messapp/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.satish.messapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -14,6 +15,11 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -22,7 +28,6 @@ public class LoginActivity extends AppCompatActivity {
 
     //UI references
     private EditText mEmail,mPassword;
-    private Button btnSignIn, btnForgotPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +35,7 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         //declare buttons and AutoCompleteTextViews in onCreate
+        Button btnSignIn, btnForgotPassword;
         mEmail = (EditText) findViewById(R.id.login_email);
         mPassword = (EditText) findViewById(R.id.login_password);
         btnSignIn = (Button) findViewById(R.id.sign_in_button);
@@ -69,8 +75,8 @@ public class LoginActivity extends AppCompatActivity {
     public void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);
+        /*FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);*/
     }
 
     private void ForgotPassword(String email) {
@@ -82,6 +88,10 @@ public class LoginActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             Log.d(TAG, "Email sent.");
                             toastMessage("Password Reset Email sent");
+                        }
+                        else {
+                            Log.d(TAG, "Email verification couldn't be sent");
+                            toastMessage("Internet Problem / Unidentified User");
                         }
                     }
                 });
@@ -112,17 +122,51 @@ public class LoginActivity extends AppCompatActivity {
 
     private void updateUI(FirebaseUser user) {
         if (user != null) {
-            // Name, email address, and profile photo Url
-            String name = user.getDisplayName();
-            String email = user.getEmail();
+            // email address
+            final String email = user.getEmail();
 
             // Check if user's email is verified
-            boolean emailVerified = user.isEmailVerified();
+            //boolean emailVerified = user.isEmailVerified();
 
             // The user's ID, unique to the Firebase project. Do NOT use this value to
             // authenticate with your backend server, if you have one. Use
             // FirebaseUser.getToken() instead.
-            String uid = user.getUid();
+            //String uid = user.getUid();
+
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            final DatabaseReference myRef = database.getReference();
+            // Read from the database
+            myRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    // This method is called once with the initial value and again
+                    // whenever data at this location is updated.
+                    int contractor = 0;
+
+                    for(DataSnapshot ds : dataSnapshot.child("users").child("contractor").getChildren()){
+                        String emailID = (String) ds.getValue();
+                        if(emailID != null && email.equals(emailID)) {
+                            contractor = 1;
+                            break;
+                        }
+                    }
+
+                    if(contractor == 1) {
+                        Intent intent = new Intent(LoginActivity.this,ContractorMain.class);
+                        startActivity(intent);
+                    }
+
+                    else {
+                        toastMessage("student");
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    // Failed to read value
+                    Log.w(TAG, "Database error", error.toException());
+                }
+            });
         }
     }
 
